f93d6a7f8a29a05031dc2ec5a75a7df2a11b500d
joaogomes1806 <joao.perestrelo18@gmail.com>
2026-06-07 19:36:52 -0300

    Update inicial.sql

--- inicial.sql
+++ inicial.sql
@@ -2,29 +2,73 @@ drop database rap;
 create database rap;
 use rap;
 
-create table clientes(
-    id int auto_increment primary key,
-    nome varchar (100) not null,
-    idade int
+CREATE TABLE usuario (
+  id SERIAL PRIMARY KEY,
+  nome VARCHAR(100) ,
+  email VARCHAR(100) UNIQUE,
+  data_cadastro DATE DEFAULT CURRENT_DATE,
+  status BOOLEAN DEFAULT TRUE
 );
-create table carros(
-    id int auto_increment primary key,
-    nome varchar (100) not null
+  
+
+create table grupo(
+    id SERIAL PRIMARY KEY,
+    nome varchar (50),
 );
 
-create table vendas(
-    id int auto_increment primary key,
-    carro_id int,
-    cliente_id int,
-    foreign key (carro_id) references carros(id),
-    foreign key (cliente_id) references clientes(id)
+create table papel(
+    id SERIAL PRIMARY KEY,
+    nome varchar (50),
 );
+create table usuario_grupo(
+    usuario_id INT REFERENCES usuario(id),
+    grupo_id INT REFERENCES grupo(id)
+    primary key (usuario_id, grupo_id)
+);
+create table grupo_papel(
+    grupo_id INT REFERENCES grupo(id),
+    papel_id INT REFERENCES papel(id),
+    primary key (grupo_id, papel_id)
+   
+);
+create table log_login(
+    id SERIAL PRIMARY KEY,
+    usuario_id INT REFERENCES usuario(id),
+    data_hora TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    ip VARCHAR(45),
+    sucesso BOOLEAN
+);
+insert into usuario (nome, email) values 
+('João', 'joao@empresa.com'),
+('Maria', 'maria@empresa.com'),
+('Carlos', 'carlos@empresa.com');
+
+insert into grupo (nome) values 
+('administradores'),
+('vendedores'),
+('suporte');
+
+
+insert into papel (nome) values 
+('administrador'),
+('vendedor'),
+('suporte');
+
+insert into usuario_grupo (usuario_id, grupo_id) values
+(1, 1),
+(2, 2),
+(3, 3);
 
  insert into carros (nome) values ('ferrari');
  insert into clientes (nome) values ('Gauss');
  insert into vendas (carro_id, cliente_id) values (1,100);
 
+insert into log_login (usuario_id, ip, sucesso) values
+(1, '192.168.1.1', true),
+(2, '192.168.1.3', false),
+(3, '192.168.1.2', true);
 
- select * from carros;
- select * from clientes;
- select * from vendas;
+select * from grupo;
+select * from usuario;
+select * from papel;query;
+select * from usuario_grupo;
